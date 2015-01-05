a75fd256566c3dfcdbae39cf8c85de555e3ea692
Quam Sodji <qsodji@gmail.com>
2015-01-05 15:25:05 -0800

    Script to create a local admin via terminal

--- localadmin.sh
+++ localadmin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #Create a localadmin via script
-
+#Copyright by Quam Sodji 2014
 dscl . create /Users/administrator
 dscl . create /Users/administrator UserShell /bin/bash
 dscl . create /Users/administrator RealName "administrator"
