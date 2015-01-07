94bcbd51d82954a20dac080c7d62b403288163c0
killahquam <quam.sodji@gii1408bikepole.185berry.us.gree-office.net>
2015-01-07 11:55:31 -0800

    Fix sub version handling for Yosemite

--- NTPcheck.py
+++ NTPcheck.py
@@ -15,16 +15,30 @@ def check_ntp():
      result = c[1]
      return result
 
-applied_patch = ["ntp-77.1.1","ntp-88.1.1","ntp-92.5.1"]
-#Find the OS version the client is running
+#Check the version and also handles new sub versions of Yosemite
+def check_version():
+    osvers = platform.mac_ver()[0] 
+    sub_ver = osvers[3:]
+    sub_ver = float(sub_ver)
 
-osvers = platform.mac_ver()[0]
+    if sub_ver == 8.5:
+       return "Mountain Lion 10.8.5"
+    elif sub_ver == 9.5:
+       return "Mavericks 10.9.5"   
+    elif sub_ver >= 10.1:
+         return "Yosemite"
+    else:
+         return osvers
+
+print check_version()
+applied_patch = ["ntp-77.1.1","ntp-88.1.1","ntp-92.5.1"]
 
 #The NTP fix is only available for the following OSX versions
-Needed_vers = ["10.10.1", "10.9.5", "10.8.5"]
+Needed_vers = ["Mountain Lion 10.8.5", "Mavericks 10.9.5", "Yosemite"]
 
-if not osvers in Needed_vers:
-       Status = "No:OS is %s"%osvers
+running_version = check_version()
+if not running_version in Needed_vers:
+       Status = "No:OS is %s"%running_version
 else:       
      result = check_ntp()
 
