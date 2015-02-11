26b3fa4623709d069d5a634504ab0dd27f2511ac
killahquam <killah@Quam-SodjiAir.local>
2015-02-11 01:45:10 -0800

    Enables Auto Update schedule and Critical Update

---
+++ Update Checker/Update_enabler.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#Enable updates
+#Enable critical updates
+#Quam Sodji Copyright 2015
+
+defaults write /Library/Preferences/com.apple.SoftwareUpdate CriticalUpdateInstall -bool yes
+defaults write /Library/Preferences/com.apple.SoftwareUpdate ConfigDataInstall -bool yes
+schedule=$(softwareupdate --schedule on)
+
+exit 0
