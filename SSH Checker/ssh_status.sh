9666e3ebb9e32113b3dfbc3491def5be45bfc023
Quam <quam.sodji@gree.net>
2015-02-18 16:54:35 -0800

    SSH Status
    Check if sshd_config is present and if SSH is enabled, you need both
    for ssh to work

---
+++ SSH Checker/ssh_status.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+#Reports back the status of SSH Remote Login via  Attribute
+#Quam Sodji
+
+ssh_chk=`systemsetup -getremotelogin`
+ssh_conf="/etc/sshd_config"
+
+if [[ "$ssh_chk" == *On ]] && [[ -f "$ssh_conf" ]]; then
+
+   result="Enabled"
+else
+   result="Disabled"
+fi
+
+echo "<result>$result</result>"
